2067db4f2722991e8e2d79c74189101db49e4624
ArunMozhi varman <129928410+AMV0027@users.noreply.github.com>
2025-07-24 16:44:35 +0530

    update to react

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhX-RGOGgz0iZjF6xNKnzoqm6Lct0F56c",
+  authDomain: "bella-v2-dc353.firebaseapp.com",
+  projectId: "bella-v2-dc353",
+  storageBucket: "bella-v2-dc353.firebasestorage.app",
+  messagingSenderId: "440388982030",
+  appId: "1:440388982030:web:35bc54019a0dd85e15335f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
